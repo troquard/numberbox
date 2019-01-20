e68e98250eda9940eeb9a9aa93f9e4f32d132237
Nicolas Troquard <nicolas.troquard@gmail.com>
2019-01-20 20:17:19 +0100

    fixed name field in json strings

--- app/src/test/java/eu/yalacirodev/numberbox/UnitTestLevel.java
+++ app/src/test/java/eu/yalacirodev/numberbox/UnitTestLevel.java
@@ -28,7 +28,8 @@ public class UnitTestLevel {
             "],\n" +
             "\"X\" : 5,\n" +
             "\"Y\" : 2,\n" +
-            "\"bestsolution\" : 61\n" +
+            "\"bestsolution\" : 61,\n" +
+            "\"name\" : \"coton05\"\n" +
             "}";
 
     // too big to solve at the moment 2017-02-13
@@ -44,7 +45,8 @@ public class UnitTestLevel {
             "],\n" +
             "\"X\" : 5,\n" +
             "\"Y\" : 2,\n" +
-            "\"bestknownsolution\" : 60\n" +
+            "\"bestknownsolution\" : 60,\n" +
+            "\"name\" : \"coton06\"\n" +
             "}";
 
 
@@ -57,7 +59,8 @@ public class UnitTestLevel {
             "[0,0,0,0,0]\n" +
             "],\n" +
             "\"X\" : 0,\n" +
-            "\"Y\" : 0\n" +
+            "\"Y\" : 0,\n" +
+            "\"name\" : \"dashpot00\"\n" +
             "}";
 
 
@@ -70,7 +73,8 @@ public class UnitTestLevel {
             "[0,0,0,0,0]\n" +
             "],\n" +
             "\"X\" : 0,\n" +
-            "\"Y\" : 0\n" +
+            "\"Y\" : 0,\n" +
+            "\"name\" : \"dashpot01\"\n" +
             "}";
 
     String dashpot02 = "{\n" +
@@ -82,7 +86,8 @@ public class UnitTestLevel {
             "[0,0,0,0,0]\n" +
             "],\n" +
             "\"X\" : 0,\n" +
-            "\"Y\" : 0\n" +
+            "\"Y\" : 0,\n" +
+            "\"name\" : \"dashpot02\"\n" +
             "}";
 
     String candidate_dashpot03 = "{\n" +
@@ -94,7 +99,8 @@ public class UnitTestLevel {
             "[0,0,0,0,0]\n" +
             "],\n" +
             "\"X\" : 0,\n" +
-            "\"Y\" : 0\n" +
+            "\"Y\" : 0,\n" +
+            "\"name\" : \"candidate_dashpot03\"\n" +
             "}";
 
 
@@ -106,7 +112,8 @@ public class UnitTestLevel {
             "[0,0,2,0,0]\n" +
             "],\n" +
             "\"X\" : 0,\n" +
-            "\"Y\" : 0\n" +
+            "\"Y\" : 0,\n" +
+            "\"name\" : \"full00\"\n" +
             "}";
 
 
@@ -118,7 +125,8 @@ public class UnitTestLevel {
             "[0,0,0,2,0]\n" +
             "],\n" +
             "\"X\" : 0,\n" +
-            "\"Y\" : 0\n" +
+            "\"Y\" : 0,\n" +
+            "\"name\" : \"full01\"\n" +
             "}";
 
     String full02 = "{\n" +
@@ -130,7 +138,8 @@ public class UnitTestLevel {
             "[0,0,0,0,0]\n" +
             "],\n" +
             "\"X\" : 2,\n" +
-            "\"Y\" : 2\n" +
+            "\"Y\" : 2,\n" +
+            "\"name\" : \"full02\"\n" +
             "}";
 
     String full03 = "{\n" +
@@ -142,7 +151,8 @@ public class UnitTestLevel {
             "[0,0,0,0,0]\n" +
             "],\n" +
             "\"X\" : 2,\n" +
-            "\"Y\" : 1\n" +
+            "\"Y\" : 1,\n" +
+            "\"name\" : \"full03\"\n" +
             "}";
 
     String full04 = "{\n" +
@@ -154,7 +164,8 @@ public class UnitTestLevel {
             "[0,2,0,0,0]\n" +
             "],\n" +
             "\"X\" : 3,\n" +
-            "\"Y\" : 2\n" +
+            "\"Y\" : 2,\n" +
+            "\"name\" : \"full04\"\n" +
             "}";
 
     String full05 = "{\n" +
@@ -166,7 +177,8 @@ public class UnitTestLevel {
             "[0,2,0,0,0]\n" +
             "],\n" +
             "\"X\" : 3,\n" +
-            "\"Y\" : 2\n" +
+            "\"Y\" : 2,\n" +
+            "\"name\" : \"full05\"\n" +
             "}";
 
     String testy01 = "{\n" +
@@ -181,7 +193,8 @@ public class UnitTestLevel {
             "],\n" +
             "\"X\" : 2,\n" +
             "\"Y\" : 2,\n" +
-            "\"bestknownsolution\" : \"169\"\n" + // thoroughness 4600 + heuristic 2
+            "\"bestknownsolution\" : 169,\n" + // thoroughness 4600 + heuristic 2
+            "\"name\" : \"testy01\"\n" +
             "}";
 
     String testy = "{\n" +
@@ -196,13 +209,14 @@ public class UnitTestLevel {
             "],\n" +
             "\"X\" : 2,\n" +
             "\"Y\" : 2,\n" +
-            "\"bestknownsolution\" : \"?\"\n" + // thoroughness ?
+            "\"bestknownsolution\" : \"?\",\n" + // thoroughness ?
+            "\"name\" : \"testy\"\n" +
             "}";
 
 
     //int MAX_CANDIDATES_PER_ROUND = Solver.ITERATIVE;
-    int MAX_CANDIDATES_PER_ROUND = 4100;
-    String json1 = testy01;
+    int MAX_CANDIDATES_PER_ROUND = 16100;
+    String json1 = coton05;
     String json2 = full04;
     String json3 = full05;
     String json4 = coton06;
